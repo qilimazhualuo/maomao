24f5b8db2bd4442d43f10a691bc920378132e796
qilimazhualuo <49083614+qilimazhualuo@users.noreply.github.com>
2024-10-20 22:35:09 +0800

    添加cesium

---
+++ src/views/about/index.vue
@@ -0,0 +1,70 @@
+<template>
+    <div class="about p-2">
+      <div>
+        <a-typography-title :level="2">关于我</a-typography-title>
+        <div class="map"></div>
+      </div>
+      <div>
+        <a-typography-title :level="2">2dmap绘制功能</a-typography-title>
+        <div class="map"></div>
+      </div>
+      <div>
+        <a-typography-title :level="2">3dmap绘制功能</a-typography-title>
+        <div class="map"></div>
+      </div>
+      <div>
+        <a-typography-title :level="2">3dmap绘制功能</a-typography-title>
+        <div class="map"></div>
+      </div>
+      <div>
+        <a-typography-title :level="2">3dmap绘制功能</a-typography-title>
+        <div class="map"></div>
+      </div>
+      <div>
+        <a-typography-title :level="2">3dmap绘制功能</a-typography-title>
+        <div class="map"></div>
+      </div>
+      <div>
+        <a-typography-title :level="2">3dmap绘制功能</a-typography-title>
+        <div class="map"></div>
+      </div>
+      <div>
+        <a-typography-title :level="2">3dmap绘制功能</a-typography-title>
+        <div class="map"></div>
+      </div>
+      <div>
+        <a-typography-title :level="2">3dmap绘制功能</a-typography-title>
+        <div class="map"></div>
+      </div>
+  
+      <div>
+        <a-typography-title :level="2">3dmap绘制功能</a-typography-title>
+        <div class="map"></div>
+      </div>
+      <div>
+        <a-typography-title :level="2">3dmap绘制功能</a-typography-title>
+        <div class="map"></div>
+      </div>
+      <div>
+        <a-typography-title :level="2">3dmap绘制功能</a-typography-title>
+        <div class="map"></div>
+      </div>
+      <div>
+        <a-typography-title :level="2">3dmap绘制功能</a-typography-title>
+        <div class="map"></div>
+      </div>
+    </div>
+  </template>
+  
+  <style>
+  .about {
+    max-width: 100%;
+    height: 100%;
+    overflow: auto;
+    .map {
+      position: relative;
+      /* height: 40vh; */
+    }
+  }
+  </style>
+  
