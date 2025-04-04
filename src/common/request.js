49b6786826fffb8b6b97fdcce41289e2a49c11eb
qilimazhualuo <49083614+qilimazhualuo@users.noreply.github.com>
2025-04-04 15:33:58 +0800

    请求修改

--- src/common/request.js
+++ src/common/request.js
@@ -3,7 +3,11 @@ import { invoke } from '@tauri-apps/api/core'
 export const request = ({ url, method = 'get', params = {}, data = {}, headers = {}, timeout = 5000 }) => {
     return new Promise((resolve, reject) => {
         url = url.includes('http') ? url : `${baseUrl}${url}`
-        invoke('http_request', { url: url, options: { method, params, body: data, headers } }).then((res) => {
+        const options = { method, params, body: data, headers }
+        if (method === 'get' || method === 'GET') {
+            delete options.body
+        }
+        invoke('http_request', { url: url, options }).then((res) => {
             resolve(res)
         }).catch((err) => {
             reject(err)
